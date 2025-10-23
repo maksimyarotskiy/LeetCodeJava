6102fbf7d89f6e38eba479edd6e979e8c73cbdea
Maksim Yartoskiy <maksimyarotskiy08@gmail.com>
2025-10-23 20:59:42 +0300

    P340

--- src/main/java/com/maksim/Main.java
+++ src/main/java/com/maksim/Main.java
@@ -5,11 +5,10 @@ import com.maksim.stage1.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {2,3,1,2,4,3};
-        int target = 7;
+        String s = "aaabbbcccddddee";
+        int k = 2;
 
-        int res = Prob209.minSubArrayLen(nums, target);
+        int res = Prob340.findLongestSubstring(s, k);
         System.out.println(res);
-
     }
 }
--- src/main/java/com/maksim/stage1/Prob340.java
+++ src/main/java/com/maksim/stage1/Prob340.java
@@ -7,11 +7,27 @@ public class Prob340 {
     public static int findLongestSubstring(String s, int k) {
         int left = 0;
         Map<Character, Integer> map = new HashMap<>();
-    
-        for (int right = 0; right < s.length(); right++) {
+        int maxLength = 0;
+        int right;
+
+        for (right = 0; right < s.length(); right++) {
             map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
+            if (map.size() > k) {
+                while (map.size() > k) {
+                    if (map.get(s.charAt(left)) == 1) {
+                        map.remove(s.charAt(left));
+                    } else {
+                        map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
+                    }
+                }
+                left++;
+                if (maxLength < (right - left + 1)) {
+                    maxLength = right - left + 1;
+                }
+            }
+
         }
 
-        return 1;
+        return maxLength;
     }
 }
