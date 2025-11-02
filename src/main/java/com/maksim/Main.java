f71bce2ba9e5a7e55173d034b128a5471525610b
Maksim Yarotskiy <maksimyarotskiy08@gmail.com>
2025-11-03 00:36:35 +0300

    Prob33

--- src/main/java/com/maksim/Main.java
+++ src/main/java/com/maksim/Main.java
@@ -1,15 +1,15 @@
 package com.maksim;
 
-import com.maksim.stage1.*;
 import com.maksim.stage2.*;
 
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {4, 5, 6, 7, 0, 1, 2};
-        int res = Prob153.findMin(nums);
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+        int res = Prob33.search(nums, target);
 
-        System.out.println(res);
+        System.out.println(res);//4
 
     }
 }
